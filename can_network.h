4a0532eb4ded66aab51d5c0f8ddcb8a87b741989
Bitiquinho <consoni_2519@hotmail.com>
2017-03-05 19:26:05 -0300

    CMake project working

--- can_network.h
+++ can_network.h
@@ -3,20 +3,20 @@
 //  Original work Copyright (c) 2012 Guilherme Fernandes                                //
 //  Modified work Copyright (c) 2016-2017 Leonardo Consoni <consoni_2519@hotmail.com>   //
 //                                                                                      //
-//  This file is part of RobRehabSystem.                                                //
+//  This file is part of Signal-IO-NIXNET.                                              //
 //                                                                                      //
-//  RobRehabSystem is free software: you can redistribute it and/or modify              //
+//  Signal-IO-NIXNET is free software: you can redistribute it and/or modify            //
 //  it under the terms of the GNU Lesser General Public License as published            //
 //  by the Free Software Foundation, either version 3 of the License, or                //
 //  (at your option) any later version.                                                 //
 //                                                                                      //
-//  RobRehabSystem is distributed in the hope that it will be useful,                   //
+//  Signal-IO-NIXNET is distributed in the hope that it will be useful,                 //
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of                      //
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                        //
 //  GNU Lesser General Public License for more details.                                 //
 //                                                                                      //
 //  You should have received a copy of the GNU Lesser General Public License            //
-//  along with RobRehabSystem. If not, see <http://www.gnu.org/licenses/>.              //
+//  along with Signal-IO-NIXNET. If not, see <http://www.gnu.org/licenses/>.            //
 //                                                                                      //
 //////////////////////////////////////////////////////////////////////////////////////////
 
--- signal_io_async.c
+++ signal_io_async.c
@@ -2,26 +2,26 @@
 //                                                                            //
 //  Copyright (c) 2016-2017 Leonardo Consoni <consoni_2519@hotmail.com>       //
 //                                                                            //
-//  This file is part of RobRehabSystem.                                      //
+//  This file is part of Signal-IO-NIXNET.                                    //
 //                                                                            //
-//  RobRehabSystem is free software: you can redistribute it and/or modify    //
+//  Signal-IO-NIXNETs free software: you can redistribute it and/or modify    //
 //  it under the terms of the GNU Lesser General Public License as published  //
 //  by the Free Software Foundation, either version 3 of the License, or      //
 //  (at your option) any later version.                                       //
 //                                                                            //
-//  RobRehabSystem is distributed in the hope that it will be useful,         //
+//  Signal-IO-NIXNET is distributed in the hope that it will be useful,       //
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of            //
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              //
 //  GNU Lesser General Public License for more details.                       //
 //                                                                            //
 //  You should have received a copy of the GNU Lesser General Public License  //
-//  along with RobRehabSystem. If not, see <http://www.gnu.org/licenses/>.    //
+//  along with Signal-IO-NIXNET. If not, see <http://www.gnu.org/licenses/>.  //
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
 
 
 #include "signal_io/interface.h"
-#include "signal_io/ni_can_epos/can_network.h"
+#include "can_network.h"
 
 #include "klib/khash.h"
 
